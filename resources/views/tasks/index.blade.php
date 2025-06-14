b8acea02c3ac722de3bbf7af53096935386aaea8
Developer-Selva <selvakumar.r.fsd@gmail.com>
2025-06-14 14:18:30 +0530

    index chagnes

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            🗂️ Task Manager (Manage Your Tasks)
+            🗂️ Task Manager
         </h2>
     </x-slot>
 
